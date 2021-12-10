35a40c5a8b9ca745d61ceddaf0929c21b3282d2d
Imran <haciyev008@gmail.com>
2021-12-10 21:56:40 +0400

    Initial Commit

---
+++ Meowclavas/MeowclavasApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeowclavasApp.swift
+//  Meowclavas
+//
+//  Created by Imran on 10.12.21.
+//
+
+import SwiftUI
+
+@main
+struct MeowclavasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
